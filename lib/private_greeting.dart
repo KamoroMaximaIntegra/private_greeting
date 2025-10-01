e4c0fd601c8ae9cda9e0f61e87e247857ae4d244
Candra Kurniawan <candra_kurniawan@kamoro.com>
2025-10-01 10:07:44 +0700

    feat: initial commit for private greeting package

---
+++ lib/private_greeting.dart
@@ -0,0 +1,7 @@
+library;
+
+/// Returns a greeting from the private package.
+String getGreeting(String organizationName) {
+  // Output dari paket ini sengaja dibuat dalam Bahasa Inggris
+  return "Hello from the private package for $organizationName!";
+}
