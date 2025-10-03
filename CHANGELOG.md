2040d49d624f175a8c1ab509f22a463be1c8bdca
Candra Kurniawan <candra_kurniawan@kamoro.com>
2025-10-03 14:32:00 +0700

    feat: add automatic publish workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,4 @@
-## 0.0.1
+## 1.0.0
 
-# 1.0.0
-
-Initial release.
-
-* TODO: Describe initial release.
+* Initial release of the package.
+* Added getGreeting function.
--- lib/private_greeting.dart
+++ lib/private_greeting.dart
@@ -3,5 +3,5 @@ library;
 /// Returns a greeting from the private package.
 String getGreeting(String organizationName) {
   // Output dari paket ini sengaja dibuat dalam Bahasa Inggris
-  return "Hello from the private package for $organizationName!";
+  return "Hello from v1.0.0 the private package for $organizationName!";
 }
